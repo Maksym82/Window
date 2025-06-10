ae8edee462908bd071dab7c7b1b63254a797375e
Maksym <112901851+Maksym82@users.noreply.github.com>
2025-06-10 15:18:01 +0300

    Update script

--- src/js/modules/tabs.js
+++ src/js/modules/tabs.js
@@ -1,4 +1,3 @@
-
 const tabs = (headerSelector, tabSelector, contentSelector, activeClass) => {
     const header = document.querySelector(headerSelector),
           tab = document.querySelectorAll(tabSelector),
